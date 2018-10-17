f807f92696525ddd46957a59774735a06cc3f0c6
isaacPowrie <35270773+isaacPowrie@users.noreply.github.com>
2018-10-16 21:54:24 -0400

    Update theArc.c

--- theArc.c
+++ theArc.c
@@ -6,54 +6,54 @@
 #include <stdio.h>
 #include "functions.h"
 
-#define GRAV 9.8
+/* #define GRAV 9.8
 #define PI 3.14159
 #define ACCTIME 3
 
 #define ERROR 0.00000000001
-#define TIMESCALE 1
+#define TIMESCALE 0.01 */
 
 int main(void)
 {
-  int angle, m, F;
-  double sine, cosine, x, y, v, dy, dx, secs;
-  double height = 0;
-  double distance = 0;
-  double maxHeight = 0;
+	int angle, m, F;
+	double sine, cosine, v, dy, dx;
+	double height = 0;
+	double distance = 0;
+	double maxHeight = 0;
 
-  // Initialize User Inputs
-  {
-    printf("What is the mass of the projectile? ");
-    scanf("%d", &m);
+	// Initialize User Inputs
+	{
+		printf("What is the mass of the projectile? ");
+		scanf("%d", &m);
 
-    printf("\n What is the force of the acceleration? ");
-    scanf("%d", &F);
+		printf("\n What is the force of the acceleration? ");
+		scanf("%d", &F);
 
-    printf("\n What is the angle of trajectory (degrees)? ");
-    scanf("%d", &angle);
-  }
+		printf("\n What is the angle of trajectory (degrees)? ");
+		scanf("%d", &angle);
+	}
 
-  // Prelimenary Calculations
-  {
-    v = velocity(F, m);
-    sine = sineOf(angle);
-    cosine = cosineOf(angle);
-    setChangeVars(v, sine, cosine, &dx, &secs);
-  }
+	// Prelimenary Calculations
+	{
+		v = velocity(F, m);
+		sine = sineOf(angle);
+		cosine = cosineOf(angle);
+		setChangeVars(v, sine, cosine, &dx, &dy);
+	}
 
-  // Calculate Points of Arc
-  {
-    do {
-      printf("(%lf, %lf) ", height, distance);
-      height += dx;
-      distance+= TIMESCALE;
-      maxHeight = maxHeight < height ? height : maxHeight;
-      dx = Decel(dx, secs);
-    } while (height >= 0);
-  }
+	// Calculate Points of Arc
+	{
+		do {
+			printf("(%lf, %lf) ", height, distance);
+			dx = decel(dx, dy, v, sine, cosine);
+			height += dx;
+			distance += dy;
+			maxHeight = maxHeight < height ? height : maxHeight;
+		} while (height >= 0);
+	}
 
-  printf("\nTotal Distance: %.2lf\n", distance);
-  printf("\nMaximum Height: %.2lf\n", maxHeight);
+	printf("\nTotal Distance: %.2lf\n", distance);
+	printf("\nMaximum Height: %.2lf\n", maxHeight);
 
-  return 0;
+	return 0;
 }
